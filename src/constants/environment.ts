44930bec919129c371ab037a8bd4027043986beb
JosepDev23 <josepdev23@gmail.com>
2024-01-20 01:02:12 +0100

    fix

---
+++ src/constants/environment.ts
@@ -0,0 +1,17 @@
+const ENV = {
+  user: {
+    register:
+      'https://software-chronicles-backend-production.up.railway.app/user/register',
+    login:
+      'https://software-chronicles-backend-production.up.railway.app/user/login',
+  },
+  entry: {
+    getByUserId:
+      'https://software-chronicles-backend-production.up.railway.app/entry/userId/',
+    getLastest:
+      'https://software-chronicles-backend-production.up.railway.app/entry/lastest/',
+    post: 'https://software-chronicles-backend-production.up.railway.app/entry',
+  },
+}
+
+export default ENV
