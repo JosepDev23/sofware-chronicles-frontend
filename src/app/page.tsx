b9097bb0656ee09e8ed6845ac07de78b92d31357
JosepDev23 <josepdev23@gmail.com>
2024-01-22 20:48:01 +0100

    three columns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 import style from './page.module.css'
 import EntryCard from '@/components/entry-card/entry-card'
@@ -8,24 +8,102 @@ import EntryService from '@/services/entry/entry-service'
 
 export default function Home() {
   const [lastestEntries, setLastestEntries] = useState<Entry[]>([])
+  const [entriesColumnOne, setEntriesColumnOne] = useState<Entry[]>([])
+  const [entriesColumnTwo, setEntriesColumnTwo] = useState<Entry[]>([])
+  const [entriesColumnThree, setEntriesColumnThree] = useState<Entry[]>([])
+
+  const refOne = useRef<HTMLDivElement>(null)
+  const refTwo = useRef<HTMLDivElement>(null)
+  const refThree = useRef<HTMLDivElement>(null)
+
+  //TODO: fix double useeffect
+  let firstTime = true
 
   useEffect(() => {
-    handleSetLastestEntries()
+    if (firstTime) {
+      ;(async () => {
+        const retrievedEntries = await EntryService.getLastestEntries()
+        setLastestEntries(retrievedEntries)
+      })()
+      firstTime = false
+    }
   }, [])
 
-  async function handleSetLastestEntries() {
-    const retrievedEntries = await EntryService.getLastestEntries()
+  useLayoutEffect(() => {
+    if (lastestEntries.length !== 0) {
+      const colNumber: number = getNextColNumber()
+      switch (colNumber) {
+        case 1:
+          setEntriesColumnOne((prev) => prev.concat(lastestEntries[0]))
+          break
+        case 2:
+          setEntriesColumnTwo((prev) => prev.concat(lastestEntries[0]))
+          break
+        case 3:
+          setEntriesColumnThree((prev) => prev.concat(lastestEntries[0]))
+          break
+      }
+      setLastestEntries((prev) =>
+        prev.filter((entry) => entry._id !== lastestEntries[0]._id)
+      )
+    }
+  }, [lastestEntries])
 
-    setLastestEntries(retrievedEntries)
+  function getNextColNumber(): number {
+    let colNumber = 1
+    if (
+      refOne.current?.getBoundingClientRect().y! <=
+      refTwo.current?.getBoundingClientRect().y!
+    ) {
+      colNumber = 1
+      if (
+        refOne.current?.getBoundingClientRect().y! <=
+        refThree.current?.getBoundingClientRect().y!
+      ) {
+        colNumber = 1
+      } else {
+        colNumber = 3
+      }
+    } else {
+      colNumber = 2
+      if (
+        refTwo.current?.getBoundingClientRect().y! <=
+        refThree.current?.getBoundingClientRect().y!
+      ) {
+        colNumber = 2
+      } else {
+        colNumber = 3
+      }
+    }
+    return colNumber
   }
 
   return (
     <div className={style.home_wrapper}>
-      {lastestEntries.map((entry) => (
-        <div className={style.card_box}>
-          <EntryCard entry={entry} />
-        </div>
-      ))}
+      <div className={style.column_wrapper}>
+        {entriesColumnOne.map((entry) => (
+          <div className={style.card_box}>
+            <EntryCard entry={entry} />
+          </div>
+        ))}
+        <div ref={refOne}></div>
+      </div>
+      <div className={style.column_wrapper}>
+        {entriesColumnTwo.map((entry) => (
+          <div className={style.card_box}>
+            <EntryCard entry={entry} />
+          </div>
+        ))}
+        <div ref={refTwo}></div>
+      </div>
+      <div className={style.column_wrapper}>
+        {entriesColumnThree.map((entry) => (
+          <div className={style.card_box}>
+            <EntryCard entry={entry} />
+          </div>
+        ))}
+        <div ref={refThree}></div>
+      </div>
     </div>
   )
 }
