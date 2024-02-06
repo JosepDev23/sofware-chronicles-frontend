07c1aff8668f4838c192f89df627bffad37b97ef
JosepDev23 <josepdev23@gmail.com>
2024-02-06 20:45:22 +0100

    new styles entry card

--- src/components/entry-card/entry-card.tsx
+++ src/components/entry-card/entry-card.tsx
@@ -10,10 +10,18 @@ const EntryCard: React.FC<EntryCardProps> = (props) => {
   return (
     <article className={style.entry_card_wrapper}>
       <div className={style.header_wrapper}>
-        <label>{`${props.entry.date.toLocaleString()}`}</label>
+        <label>{`${props.entry.date.toLocaleDateString(undefined, {
+          year: 'numeric',
+          month: 'long',
+          day: '2-digit',
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+          hourCycle: 'h23',
+        })}`}</label>
       </div>
       <div className={style.body_wrapper}>
-        <label>{props.entry.body}</label>
+        <label className={style.text_body}>{props.entry.body}</label>
       </div>
     </article>
   )
